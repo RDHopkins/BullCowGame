0586ea363d3534168c3d83381753805680095c89
Ryan Hopkins <Ryan.Hopkins@snhu.edu>
2018-09-02 18:16:44 -0400

    Add introduction, input and output statements
    Update main.cpp to include a game introduction and directions, ask the user for a word, read the word into a string and print it back to the user.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    constexpr int WORD_LENGTH = 5;
+
+    // Introduction
+    std::cout << "Welcome to Bulls and Cows" << std::endl;
+    std::cout << "I'm thinking of a " << WORD_LENGTH << " letter isogram, can you guess it?" << std::endl;
+    
+    // Get a guess from the player
+    std::string Guess = "";
+    std::cout << "Enter your guess: ";
+    std::getline(std::cin, Guess);
+    std::cout << std::endl;
+    
+    // Repeat guess back to player
+    std::cout << "You guessed: " << Guess << std::endl;
+    
+    std::cout << std::endl;
     return 0;
 }
